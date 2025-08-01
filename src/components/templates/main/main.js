149890442757cd95fd473e32069b1caffac98562
MaxBondar1990 <maksim.visualstudiocode.1990@gmail.com>
2025-08-01 14:42:13 +0300

    upd_goods

--- src/components/templates/main/main.js
+++ src/components/templates/main/main.js
@@ -3,6 +3,8 @@
 import { searchClean, searchView, search } from '../../layout/header/header.js'
 import { handleMainMenuClick } from '../../layout/navigation/navigation.js'
 import { burgerClick } from '../../layout/leftSideBar/leftSideBar.js'
+import { viewColorItems } from '../../layout/goods/goods.js'
+
 
 document.addEventListener('click', (event) => {
    searchClean(event)
@@ -11,6 +13,8 @@ document.addEventListener('click', (event) => {
    burgerClick(event);
 
    handleMainMenuClick(event);
+
+   viewColorItems(event);
 });
 
 document.addEventListener('input', (event) => {
