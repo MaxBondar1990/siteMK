fd6940774946ddf4daea7667a761456ad5b14424
MaxBondar1990 <maksim.visualstudiocode.1990@gmail.com>
2025-08-08 18:26:42 +0300

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,12 @@
 import { fetchHTML, closeModal, removeModal } from '../components/custom/featch/featch.js'
+import { sendForm, isRequired } from '../components/custom/sendform/sendform.js'
+
 
 document.addEventListener('click', (event) => {
    fetchHTML(event);
    closeModal(event);
    removeModal(event);
+
+   sendForm(event);
+   isRequired(form);
 });
