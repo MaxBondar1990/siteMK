4c36f1c5612ccf659934e5b76057c2116babaa35
MaxBondar1990 <maksim.visualstudiocode.1990@gmail.com>
2025-08-26 21:07:52 +0300

    upd

--- src/components/custom/showtextbtn/showtextbtn.js
+++ src/components/custom/showtextbtn/showtextbtn.js
@@ -1,1 +1,31 @@
 import "./showtextbtn.scss"
+
+// export function openFuleContentWelcomTxt (event) {
+//    console.log('123123123')
+//    const btn = event.target.closest('[data-btn="action"]');
+//    if (btn) {
+//       const section = event.target.closest('[data-btn="action"]');
+//       section.classList.add('_down-arrow');
+//    }
+// }
+
+export function openFuleContentWelcomTxt (event) {
+   const btn = event.target.closest('[data-btn="action"]');
+   if (btn) {
+      const section = btn.closest('[data-name="action"]');
+      section.classList.toggle('_action');
+   }
+}
+
+export function classReplacement(event) {
+   const btn = event.target.closest('[data-btn="action"]');
+   if (btn) {
+      if (btn.classList.contains('_down-arrow')) {
+         btn.classList.remove('_down-arrow');
+         btn.classList.add('_up-arrow');
+      } else {
+         btn.classList.remove('_up-arrow');
+         btn.classList.add('_down-arrow');
+      }
+   }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 import { fetchHTML, closeModal, removeModal } from '../components/custom/featch/featch.js'
 import { sendForm } from '../components/custom/sendform/sendform.js'
 
+import { openFuleContentWelcomTxt, classReplacement } from '../components/custom/showtextbtn/showtextbtn.js'
+
+
 
 document.addEventListener('click', (event) => {
    fetchHTML(event);
@@ -8,5 +11,8 @@ document.addEventListener('click', (event) => {
    removeModal(event);
 
    sendForm(event);
+
+   openFuleContentWelcomTxt(event);
+   classReplacement(event);
 
 });
