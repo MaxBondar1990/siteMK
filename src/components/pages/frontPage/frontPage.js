00750ead3833ce49358c9efe213334cc8f37488a
MaxBondar1990 <maksim.visualstudiocode.1990@gmail.com>
2025-09-09 11:13:29 +0300

    upd

--- src/components/pages/frontPage/frontPage.js
+++ src/components/pages/frontPage/frontPage.js
@@ -1,10 +1,10 @@
-import { viewColorItems, applyColorAndPriceChange } from '../../layout/catalog/catalog.js'
+// import { viewColorItems, applyColorAndPriceChange } from '../../layout/catalog/catalog.js'
 
-document.addEventListener('click', (event) => {
+// document.addEventListener('click', (event) => {
 
-   viewColorItems(event);
-   applyColorAndPriceChange(event);
+//    viewColorItems(event);
+//    applyColorAndPriceChange(event);
 
 
-});
+// });
 
