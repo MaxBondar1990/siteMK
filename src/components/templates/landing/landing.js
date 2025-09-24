2ec071502d736ffbc3c037e90b4aa0b4ee9ac73f
MaxBondar1990 <maksim.visualstudiocode.1990@gmail.com>
2025-09-24 14:44:34 +0300

    upd

--- src/components/templates/landing/landing.js
+++ src/components/templates/landing/landing.js
@@ -1,17 +1,17 @@
-import { searchClean, searchView, search } from '../../layout/header/header.js'
-import { viewNavModalGroupMenu, closeNavModalGroupMenu } from '../../layout/navigation/navigation.js'
-import { burgerClick } from '../../layout/leftSideBar/leftSideBar.js'
+// import { searchClean, searchView, search } from '../../sections/header/header.js'
+// import { viewNavModalGroupMenu, closeNavModalGroupMenu } from '../../sections/navigation/navigation.js'
+// import { burgerClick } from '../../sections/leftSideBar/leftSideBar.js'
 
-document.addEventListener('click', (event) => {
-   searchClean(event)
-   searchView(event)
+// document.addEventListener('click', (event) => {
+//    searchClean(event)
+//    searchView(event)
 
-   burgerClick(event);
+//    burgerClick(event);
 
-   viewNavModalGroupMenu(event);
-   closeNavModalGroupMenu(event);
-});
+//    viewNavModalGroupMenu(event);
+//    closeNavModalGroupMenu(event);
+// });
 
-document.addEventListener('input', (event) => {
-   search(event)
-});
+// document.addEventListener('input', (event) => {
+//    search(event)
+// });
